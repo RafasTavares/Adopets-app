a6c9100562c27e0ac6cfb2485c7f7e5ce4219410
Rafael Tavares Andrade Toledo <rafaeltavaresandrade@gmail.com>
2018-07-31 01:26:27 -0300

    add js

--- src/components/steps.js
+++ src/components/steps.js
@@ -5,20 +5,14 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Steps, { Step } from 'rc-steps';
 
-//const container = document.getElementById('root');
-
-const description = 'descrição steps';
-
 class StepsComponent extends Component {
 
     render() {
         return (
             <Steps progressDot size="small" current={1}>
-                <Step title="1" description={description} />
-                <Step title="2" description={description} />
-                <Step title="3" description={description} />
-                <Step title="4" description={description} />
-                <Step title="5" description={description} />
+                <Step />
+                <Step />
+                <Step />
             </Steps>
         );
     }
